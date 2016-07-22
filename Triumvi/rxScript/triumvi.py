01089a50f1b865e648a1428a69be96d742cc0682
Ye-Sheng Kuo <higherkuo@gmail.com>
2016-07-22 18:36:17 -0400

    add triumvi scripts to support decoding PF, VRMS, IRMS This binary / script supports old triumvi (w/o PF... etc) but the old binary / script cannot decode the extended packet

--- Triumvi/rxScript/triumvi.py
+++ Triumvi/rxScript/triumvi.py
@@ -10,14 +10,19 @@ import threading
 
 CC2538INTPINNUM = 38 # MRAA number, GP43
 CC2538RESETPINNUM = 51 # MRAA number, GP41
-MAX_TRIUMVI_PKT_LEN = 16 # maximum triumvi packet length
-MIN_TRIUMVI_PKT_LEN = 14 # maximum triumvi packet length
+MAX_TRIUMVI_PKT_LEN = 28 # maximum triumvi packet length
+MIN_TRIUMVI_PKT_LEN = 14 # minimum triumvi packet length
 MAX_FLUSH_THRESHOLD = 32 # maximum trials before reset cc2538
 
 gateway_id = ':'.join(['{:02x}'.format((uuid.getnode() >> i) & 0xff) for i in range(0,8*6,8)][::-1])
 
 
 condition = threading.Condition()
+
+def unpack(data):
+    return float(data[0] + (data[1]<<8) + (data[2]<<16) + (data[3]<<24))
+
+# 1 sign bit, 8 exp, 23 fraction
 
 def triumviCallBackISR(args):
     # args.cc2538ISR()
@@ -34,7 +39,7 @@ class triumviPacket(object):
         self._DISPLAYORDER = \
         ['Packet Type', 'Source Addr', 'Power', \
         'External Voltage Waveform', 'Battery Pack Attached', 'Three Phase Unit', \
-        'Frame Write', 'Panel ID', 'Circuit ID']
+        'Frame Write', 'Panel ID', 'Circuit ID', 'Power Factor', 'VRMS', 'IRMS']
 
         if data[0] == self._TRIUMVI_PKT_ID:
             self.dictionary['Packet Type'] = 'Triumvi Packet'
@@ -45,19 +50,24 @@ class triumviPacket(object):
 
         # self.dictionary['Source Addr'] = [hex(i) for i in data[1:9]]
         device_id = ''.join(['{:02x}'.format(i) for i in data[1:9]])
-        self.dictionary['Power'] = (data[9] + (data[10]<<8) + (data[11]<<16) + (data[12]<<24))/1000
+        self.dictionary['Power'] = unpack(data[9:13])/1000
+        offset = 14
         if self.dictionary['Packet Type'] == 'Triumvi Packet':
             if data[13] & 128:
                 self.dictionary['External Voltage Waveform'] = True
             if data[13] & 64:
                 self.dictionary['Battery Pack Attached'] = True
+                self.dictionary['Panel ID'] = data[offset]
+                self.dictionary['Circuit ID'] = data[offset+1]
+                offset += 2
             if data[13] & 48:
                 self.dictionary['Three Phase Unit'] = True
             if data[13] & 8:
                 self.dictionary['Fram Write'] = True
-        if self.dictionary.get('Battery Pack Attached', False):
-            self.dictionary['Panel ID'] = data[14]
-            self.dictionary['Circuit ID'] = data[15]
+            if data[13] & 4:
+                self.dictionary['Power Factor'] = unpack(data[offset:offset+4])/1000
+                self.dictionary['VRMS'] = unpack(data[offset+4:offset+8])/1000
+                self.dictionary['IRMS'] = unpack(data[offset+8:offset+12])/1000
 
         self.dictionary['_meta'] = {
             'received_time': datetime.utcnow().isoformat(),
