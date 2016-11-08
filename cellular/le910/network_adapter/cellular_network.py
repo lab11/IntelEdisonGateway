4db7fd722e1deebccedb47e566c1d3b6badeda12
Ye-Sheng Kuo <higherkuo@gmail.com>
2016-11-08 15:14:56 -0500

    add qmi-network check

--- cellular/le910/network_adapter/cellular_network.py
+++ cellular/le910/network_adapter/cellular_network.py
@@ -4,6 +4,8 @@ import sys
 import os
 import stat
 
+TELIT_VENDOR_ID = '1bc7'
+
 def main():
     if len(sys.argv) != 3 or (sys.argv[2] != 'start' and sys.argv[2] != 'stop'):
         print('Invalid syntax')
@@ -12,6 +14,16 @@ def main():
         sys.exit(0)
     myDev = sys.argv[1]
     op = sys.argv[2]
+
+    # check if USB is attached
+    p1 = subprocess.Popen(["lsusb"], stdout=subprocess.PIPE)
+    p2 = subprocess.Popen(["grep", TELIT_VENDOR_ID], stdin=p1.stdout, stdout=subprocess.PIPE)
+    p1.stdout.close()
+    output = p2.communicate()[0]
+    if len(output) == 0:
+        print('Modem is not found')
+        sys.exit(0)
+
 
     # check if libqmi is install
     p1 = subprocess.Popen(["ldconfig", "-p"], stdout=subprocess.PIPE)
@@ -39,9 +51,10 @@ def main():
             sys.exit(0)
 
         # bring the interface up
-        if subprocess.call(['ifconfig', ifname, 'up']) != 0:
+        res = subprocess.call(['ifconfig', ifname, 'up'])
+        if res != 0:
             print('Cannot bring the network {:} up'.format(ifname))
-            sys.exit(0)
+            sys.exit(res)
 
         # set mode
         sysoutput = subprocess.check_output(['qmicli', '-d', myDev, '--dms-set-operating-mode=online'])
@@ -54,19 +67,26 @@ def main():
             print('Cannot find /etc/qmi-network.conf file')
             sys.exit(0)
         
+        # try to connect to network
         print('Starting network...')
-        subprocess.call(['qmi-network', myDev, 'start'])
+        res = subprocess.call(["qmi-network", myDev, 'start'])
+        if res != 0:
+            print('Failed to connect to network')
+            sys.exit(res)
 
+        # obtain IP address
         print('Acquiring IP address...')
         sysout = subprocess.check_output(['udhcpc', '-i', ifname])
+
     elif op == 'stop':
         print('Stopping network...')
         subprocess.call(['qmi-network', myDev, 'stop'])
 
         # bring the interface down
-        if subprocess.call(['ifconfig', ifname, 'down']) != 0:
+        res = subprocess.call(['ifconfig', ifname, 'down'])
+        if res != 0:
             print('Cannot bring the network {:} up'.format(ifname))
-            sys.exit(0)
+            sys.exit(res)
 
 
 
